34d9cc2592b5acdee47b75de2b15ef3ec130d5b6
HIRDI, LASZLO <laszlo.hirdi@lhsystems.com>
2024-03-31 08:41:51 +0200

    use ejml

--- src/main/java/org/sodoku/SodokuTable.java
+++ src/main/java/org/sodoku/SodokuTable.java
@@ -4,6 +4,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.ejml.simple.SimpleMatrix;
+
 public class SodokuTable {
 
   private static final int SODOKU_SIZE = 9;
@@ -13,17 +15,13 @@ public class SodokuTable {
   private static final Set<Integer> ALL_ELEMENTS =
       IntStream.rangeClosed(1, SODOKU_SIZE).boxed().collect(Collectors.toSet());
 
-  private final int[][] table = new int[SODOKU_SIZE][SODOKU_SIZE];
+  private final SimpleMatrix table = new SimpleMatrix(SODOKU_SIZE, SODOKU_SIZE);
 
   public static SodokuTable generate() {
-    int tryCount = 0;
     Random random = new Random();
     while (true) {
-      ++tryCount;
-      ;
       try {
         SodokuTable sodokuTable = new SodokuTable();
-
         for (int row = 0; row < SODOKU_SIZE; row++) {
           for (int col = 0; col < SODOKU_SIZE; col++) {
             Set<Integer> rowElements = sodokuTable.getRowElements(row);
@@ -31,11 +29,9 @@ public class SodokuTable {
             Set<Integer> sectionElements = sodokuTable.getSectionElements(row, col);
             Set<Integer> missingElements = sodokuTable.getMissingElements(rowElements, colElements, sectionElements);
             int randomElement = getRandomElement(missingElements, random);
-            sodokuTable.table[row][col] = randomElement;
+            sodokuTable.table.set(row, col, randomElement);
           }
         }
-
-        System.out.println("Try count:" + tryCount);
         return sodokuTable;
       } catch (Exception e) {
         System.out.println("retrying...");
@@ -44,8 +40,7 @@ public class SodokuTable {
   }
 
   private static int getRandomElement(Set<Integer> set, Random random) {
-    List<Integer> list = new ArrayList<>(set);
-    return list.get(random.nextInt(list.size()));
+    return new ArrayList<>(set).get(random.nextInt(set.size()));
   }
 
   private Set<Integer> getMissingElements(Set<Integer> rowElements, Set<Integer> colElements,
@@ -58,24 +53,22 @@ public class SodokuTable {
   }
 
   private Set<Integer> getRowElements(int row) {
-    return Arrays.stream(table[row]).boxed().collect(Collectors.toSet());
+    return IntStream.range(0, SODOKU_SIZE).mapToObj(col -> (int)table.get(row, col)).collect(Collectors.toSet());
   }
 
   private Set<Integer> getColumnElements(int col) {
-    return IntStream.range(0, SODOKU_SIZE).map(i -> table[i][col]).boxed().collect(Collectors.toSet());
+    return IntStream.range(0, SODOKU_SIZE).mapToObj(row -> (int)table.get(row, col)).collect(Collectors.toSet());
   }
 
   private Set<Integer> getSectionElements(int row, int col) {
     int startRow = (row / SECTION_SIZE) * SECTION_SIZE;
     int startCol = (col / SECTION_SIZE) * SECTION_SIZE;
-
     Set<Integer> sectionElements = new HashSet<>();
     for (int i = startRow; i < startRow + SECTION_SIZE; i++) {
       for (int j = startCol; j < startCol + SECTION_SIZE; j++) {
-        sectionElements.add(table[i][j]);
+        sectionElements.add((int)table.get(i, j));
       }
     }
-
     return sectionElements;
   }
 
@@ -84,7 +77,7 @@ public class SodokuTable {
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < SODOKU_SIZE; i++) {
       for (int j = 0; j < SODOKU_SIZE; j++) {
-        sb.append(table[i][j]).append(' ');
+        sb.append((int)table.get(i, j)).append(' ');
         if ((j + 1) % SECTION_SIZE == 0) {
           sb.append(' ');
         }
